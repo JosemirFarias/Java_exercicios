4596ad18a42914ff8158fefd1fe279e9da6970e1
JosemirFarias <josemirfbrito@gmail.com>
2024-09-21 13:04:58 -0300

    Atualiza exemplos de controle

--- src/controle/DiaSemana.java
+++ src/controle/DiaSemana.java
@@ -4,30 +4,29 @@ import java.util.Scanner;
 
 public class DiaSemana {
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
+
 		System.out.print("digite o dia da semana: ");
 		String dia = entrada.next();
-		
-		if(dia.equalsIgnoreCase("domingo")) {
+		if (dia.equalsIgnoreCase("domingo")) {
 			System.out.println(1);
-		} else if(dia.equalsIgnoreCase("segunda")) {
+		} else if (dia.equalsIgnoreCase("segunda")) {
 			System.out.println(2);
-		} else if(dia.equalsIgnoreCase("terça")) {
+		} else if (dia.equalsIgnoreCase("terça")) {
 			System.out.println(3);
-		} else if(dia.equalsIgnoreCase("quarta")) {
+		} else if (dia.equalsIgnoreCase("quarta")) {
 			System.out.println(4);
-		} else if(dia.equalsIgnoreCase("quinta")) {
+		} else if (dia.equalsIgnoreCase("quinta")) {
 			System.out.println(5);
-		} else if(dia.equalsIgnoreCase("sexta")) {
+		} else if (dia.equalsIgnoreCase("sexta")) {
 			System.out.println(6);
-		} else if(dia.equalsIgnoreCase("sábado")) {
+		} else if (dia.equalsIgnoreCase("sábado")) {
 			System.out.println(7);
 		} else {
 			System.out.println("Dia inválido");
 		}
-		
+
 		entrada.close();
 	}
 
