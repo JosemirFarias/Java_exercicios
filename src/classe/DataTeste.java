f50fabdfb89360fbcc93d92bfe94e2ec75d5d72c
JosemirFarias <josemirfbrito@gmail.com>
2024-09-29 14:44:43 -0300

    Continuação Metodos.

--- src/classe/DataTeste.java
+++ src/classe/DataTeste.java
@@ -16,8 +16,8 @@ public class DataTeste {
 		d2.mes = 9;
 		d2.ano = 2024;
 		
-		System.out.printf("%d/%d/%d\n", d1.dia, d1.mes, d1.ano);
-		System.out.printf("%d/%d/%d", d2.dia, d2.mes, d2.ano);
+		System.out.printf(d1.obterDataFormatada());
+		System.out.printf(d1.obterDataFormatada());
 		
 	}
 
