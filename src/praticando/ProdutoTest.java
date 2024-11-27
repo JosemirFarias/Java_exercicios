7096f531b007e550008ff3d66170a7c699e09d85
JosemirFarias <josemirfbrito@gmail.com>
2024-11-26 21:19:42 -0300

    getters e setters

---
+++ src/praticando/ProdutoTest.java
@@ -0,0 +1,10 @@
+package praticando;
+
+public class ProdutoTest {
+    public static void main(String[] args) {
+
+        Produto p1 = new Produto("Celular", 2500);
+
+        System.out.println(p1);
+    }
+}
